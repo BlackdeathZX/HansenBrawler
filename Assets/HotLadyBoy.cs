df5132e667ae91d682d36e3bd0ce8ad9bdaf8580
3bbe <oliver.traneberg.ebsen@gmail.com>
2023-09-21 11:56:20 +0200

    yes

---
+++ Assets/HotLadyBoy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotLadyBoy : Player
+{
+    public float Hp = 100;
+    public Foot RightFoot;
+    public Foot LeftFoot;
+    public Hand RightHand;
+    public Hand LeftHand;
+    public Body Body;
+
+    
+}
